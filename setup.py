b0ecc86ce881648a317322d50d985a3cd25eb58d
Arthur Tilley <aetilley@berkeley.edu>
2015-10-10 10:30:15 -0700

    added author email

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ config = {
     'description': 'An implementation of the SigClust algorithm in Python',
     'author': 'Arthur Tilley',
     'url': 'https://github.com/aetilley/sigclust',
-    'author_email': 'My email.',
+    'author_email': 'aetilley@gmail.com',
     'version': '0.1',
     'install requires': ['nose'],
     'packages': ['sigclust'],
