bddfd0ec8af1e5df6c05f000b9e2ac7dc0304d18
Vagrant Default User <vagrant@debian-jessie.vagrantup.com>
2015-10-23 16:05:08 +0000

    Generalized utility for reading data into a np.matrix for sigclust.  read_data.py became read.py

---
+++ enwiki_data/read.py
@@ -0,0 +1,70 @@
+from sys import argv
+
+def read_data(f, features, rids=False):
+    """
+    Reads a set of features and a label from a file one row at a time.
+    rids says to expect the first column to be id numbers.
+    """
+    for line in f: # Implicitly splits rows on \n
+        parts = line.strip().split("\t") # Splits columns on \t
+
+        if rids:
+            rev_id = parts[0]
+            parts = parts[1:]
+            
+        values = parts[:-1] # All but the last column are feature values.
+        label = parts[-1] # Last column is a label
+
+        feature_values = []
+        for feature, value in zip(features, values): 
+            # Each feature knows its type and will perform the right conversion
+            
+            if feature.returns == bool:
+                # Booleans are weird.  bool("False") == True, so you need to string match "True"
+                feature_values.append(value == "True")
+            else:
+                feature_values.append(feature.returns(value))
+
+        row = feature_values[:]
+        row.append(label == "True")
+        if rids:
+            row.insert(0, int(rev_id))
+                
+        yield row
+
+
+
+from editquality.feature_lists import enwiki
+import numpy
+import sigclust.sigclust
+
+def get_mat(file_name, rids = False):
+    # Returns numpy array of input data.  Note that rows will include target labels and/or rev_ids if these are included in the original data.
+    f = open(file_name)
+
+    rows = list(read_data(f, enwiki.damaging, rids))
+
+    mat = numpy.array(rows).astype(float)
+
+    result = mat[:, :-1]
+
+    if rids:
+        result = result[:, 1:]
+    return result
+
+
+"""
+
+ Note:  currently data_mat is about half zeros.
+
+When running sigclust 30 times on normally generated data of size (20, 5) with mc_iters = 1000 and floor = 0,  the resulting p values were
+
+
+
+Out[6]: 
+array([ 0.218,  0.367,  0.656,  0.34 ,  0.014,  0.208,  0.526,  0.791,
+        0.662,  0.645,  0.128,  0.607,  0.23 ,  0.796,  0.449,  0.889,
+        0.68 ,  0.499,  0.233,  0.004])
+
+In [7]:
+"""
--- sigclust/sigclust.py
+++ sigclust/sigclust.py
@@ -2,56 +2,73 @@ import numpy as np
 from sklearn.cluster import k_means
 
 
-def sigclust(X, mc_iters=100):
+
+
+
+def sigclust(X, mc_iters=200, floor=0, verbose=True):
     """
     Returns p-value for k-means++ clustering of array X with k==2.
     X has shape (num_samples, num_features).
-    mc is an integer giving the number of iterations in 
+    mc_iters is an integer giving the number of iterations in 
     the Monte Carlo step.
+    floor is an optional minimum on simulation variances.
 
     """
     num_samples, num_features = X.shape
+    print("Number of samples: %d, Numer of features: %d" % (num_samples, num_features))
     
     ci, labels = cluster_index_2(X)
+    print("Cluster index of input data: %f" % ci)
 
-    bg_noise_var = (MAD(X)*normalizer)**2
+    mad = MAD(X)
+    print("Median absolute deviation from the median of input data:  %f" % mad)
+
+    bg_noise_var = (mad*normalizer)**2
+    print("Estimated variance for background noise: %f" % bg_noise_var)
+
+    floor_final = max(floor, bg_noise_var)
 
     data_cov_mat = np.cov(X.T)
 
     eig_vals, eig_vects = np.linalg.eig(data_cov_mat)
 
-    eig_vals.sort()
+    args = np.argsort(eig_vals)
 
-    new_vars = np.maximum(eig_vals, bg_noise_var * np.ones(num_features))
+    rev_sorted_args = args[::-1]
+    
+    rev_sorted_vals = eig_vals[rev_sorted_args]
+
+    new_vars = np.maximum(rev_sorted_vals, floor_final * np.ones(num_features))
+
+    print("Variances for simulation are:", new_vars)
 
     sim_cov_mat = np.diag(new_vars)
 
-    sim_data = np.random.multivariate_normal(np.zeros(num_features), sim_cov_mat, (mc_iters, num_samples))
-    """
-    Shape of sim_data is (mc_iters, num_samples, num_features)
-    """
+    if verbose:
+        input("Press enter to begin simulation.")
+    
+    ##MONTE CARLO STEP
 
-    ci_sims = np.zeros(mc_iters)
+    #Counter for simulated cluster indices less than or equal to ci.
+    lte = 0
     
     for i in np.arange(mc_iters):
-        ci_sims[i] = cluster_index_2(sim_data[i,:,:])[0]
+    #Generate mc_iters datasets each of the same size as the original input.
+        sim_data = np.random.multivariate_normal(np.zeros(num_features), sim_cov_mat, num_samples)
 
-    p = compute_pvalue(ci, ci_sims)
+        """
+        Shape of sim_data was (mc_iters, num_samples, num_features)
+        """
 
+        ci_sim = (cluster_index_2(sim_data))[0]
+        print("Cluster index of simulated data set number %d is %f" % (i, ci_sim))
+
+        if ci_sim <= ci:
+            lte += 1
+    #P value
+    p = lte / mc_iters
+        
     return p, labels
-
-
-
-def compute_pvalue(i, sims):
-    """
-    Returns the propositon of sims less than or equal to i
-    """
-    lower_than = sims <= i
-
-    num_lower_than = np.sum(lower_than.astype(int))
-
-    return num_lower_than / sims.size
-
 
 
 def cluster_index_2(X):
@@ -73,19 +90,21 @@ def cluster_index_2(X):
     return ci , labels
     
 
-def MAD(X):
-    """
-    Returns the median absolute deviation from the median
-    for a data array X.  If X has dimention greater than 1, 
-    returns MAD of flattened array.
-    """
-    return np.median(np.abs(X - np.median(X)))
-
-
-
 
 normalizer = 1.48257969
 """
 Equal to 1/(Phi^{-1}(3/4)) where Phi is the CDF
 of the standard normal distribution N(0, 1)
 """
+
+
+def MAD(X):
+    """
+    Returns the median absolute deviation from the median
+    for a data array X.  If X has dimension greater than 1, 
+    returns MAD of flattened array.
+    """
+    return np.median(np.abs(X - np.median(X)))
+
+
+
