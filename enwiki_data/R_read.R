4b58cd2c5a3b067020d7bd8694a1cf544dd61c33
aetilley <aetilley@gmail.com>
2015-10-28 13:35:52 -0700

    Added some tests to R_read.R.   Currently investigating this error:
    http://stackoverflow.com/questions/21382681/kmeans-quick-transfer-stage-steps-exceeded-maximum

--- enwiki_data/R_read.R
+++ enwiki_data/R_read.R
@@ -9,6 +9,30 @@ features_2[features_2 == "True"] <- 1
 features_2[features_2 == "False"] <- 0
 
 data_mat = data.matrix(features_2)
-sig_obj = sigclust(data_mat, nsim = 100, icovest = 1)
+sig_obj = sigclust(data_mat, nsim = 100, icovest = 3)
 
-print(sig_obj@pval)
+print(sprintf("pval for input data:  %f", sig_obj@pval))
+
+##TEST on random data of same dim.
+nrow = dim(data_mat)[1]
+ncol = dim(data_mat)[2]
+tot_vals = length(data_mat)
+
+rand_mat_0 = matrix(rnorm(tot_vals, mean = 0, sd = 1), nrow = nrow, ncol=ncol)
+
+sig_obj_0 = sigclust(rand_mat_0, nsim = 100, icovest = 3)
+print(sprintf("pval for random matrix of same size:  %f", sig_obj_0@pval))
+
+## Two halves from their own normal
+ncol_1_1 = floor(ncol / 2)
+ncol_1_2 = ncol - ncol_1_1
+
+rand_mat_1_1 = matrix(rnorm(nrow*ncol_1_1, mean = 0, sd = 1), nrow = nrow, ncol = ncol_1_1)
+
+
+rand_mat_1_2 = matrix(rnorm(nrow*ncol_1_2, mean = .5, sd = 1), nrow = nrow, ncol = ncol_1_2)
+
+rand_mat_1 = rbind(rand_mat_1_1, rand_mat_1_2)
+
+sig_obj_1 = sigclust(rand_mat_1, nsim= 100, icovest = 3)
+print(sprintf("Letting half of input data be from N(0,1) and hald be from N(.5, 1):  %f", sig_obj_1@pval))
